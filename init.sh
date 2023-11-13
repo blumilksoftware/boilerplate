c44f4ae39b3bdd012058e96c42da707861145179
Kamil <kamil.piech.97@gmail.com>
2023-11-13 15:25:46 +0100

    - some script fixes

--- init.sh
+++ init.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
+# Application name validation function
+isValidAppNamespace() {
+    local appName="$1"
+
+    if [ -z "$appName" ]; then
+        echo -e "${RED}Application name cannot be empty.${NC}"
+        return 1
+    fi
+
+    if ! [[ $appName =~ ^[A-Z][a-zA-Z0-9]*$ ]]; then
+        echo -e "${RED}Application name must start with an uppercase letter and be alphanumeric.${NC}"
+        return 1
+    fi
+
+    local len=${#appName}
+    if [ $len -lt 2 ] || [ $len -gt 20 ]; then
+        echo -e "${RED}Application name must be between 2 and 20 characters.${NC}"
+        return 1
+    fi
+
+    return 0
+}
+
+# Function to prompt for input
+promptForInput() {
+    read -p "Enter the app name: " APP_NAME
+    read -p "Enter the namespace for the app: " APP_NAMESPACE
+}
+
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 app_name namespace"
-    exit 1
+    echo "Arguments missing or incomplete. Please follow the prompts."
+    promptForInput
+else
+    # Assign arguments to variables
+    APP_NAME=$1
+    APP_NAMESPACE=$2
 fi
 
-# Assign argument to variables for better readability
-APP_NAME=$1
-NAMESPACE=$2
+# Validate the namespace
+if ! isValidAppNamespace "$APP_NAMESPACE"; then
+    exit 1
+fi
 
 # Current working directory
 TARGET_DIR=$(pwd)
@@ -17,7 +51,7 @@ TARGET_DIR=$(pwd)
 TEMP_DIR=$(mktemp -d)
 
 # Clone the repository into a temporary directory
-git clone --branch init "https://github.com/blumilksoftware/boilerplate" "$TEMP_DIR"
+git clone "https://github.com/blumilksoftware/boilerplate" "$TEMP_DIR"
 
 # Check if the clone was successful
 if [ $? -ne 0 ]; then
@@ -40,6 +74,6 @@ rm -rf "$TEMP_DIR"
 # Change the given string to a new string in all files in the target directory
 # This uses a loop to go through each file and sed to perform the string replacement
 find "$TARGET_DIR" -type f -exec sed -i "s/example-app/$APP_NAME/g" {} \;
-find "$TARGET_DIR" -type f -exec sed -i "s/ExampleApp/$NAMESPACE/g" {} \;
+find "$TARGET_DIR" -type f -exec sed -i "s/ExampleApp/$APP_NAMESPACE/g" {} \;
 
 echo "Operation completed."
