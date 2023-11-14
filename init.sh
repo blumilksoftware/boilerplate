102dee8084a7404acd4b4517dfd74be52787e0e5
Kamil <kamil.piech.97@gmail.com>
2023-11-14 08:00:57 +0100

    - update script

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Application name validation function
+BLUE='\033[01;34m'
+NC='\033[0m'
+
 isValidAppNamespace() {
     local appName="$1"
 
@@ -23,64 +25,57 @@ isValidAppNamespace() {
     return 0
 }
 
-# Function to prompt for input
 promptForInput() {
     read -p "Enter the app name: " APP_NAME
-    read -p "Enter the namespace for the app: " APP_NAMESPACE
+    read -p "Enter the namespace for the app (leave empty for default namespace 'App'): " APP_NAMESPACE
     read -p "Enter the branch name (leave empty for default branch): " BRANCH_NAME
-
+    APP_NAMESPACE=${APP_NAMESPACE:-App}
 }
 
-# Check if the correct number of arguments is provided
-if [ "$#" -lt 2 ]; then
+echo -e "${BLUE}
+██████╗ ██╗     ██╗   ██╗███╗   ███╗██╗██╗     ██╗  ██╗    ██████╗  ██████╗ ██╗██╗     ███████╗██████╗ ██████╗ ██╗      █████╗ ████████╗███████╗
+██╔══██╗██║     ██║   ██║████╗ ████║██║██║     ██║ ██╔╝    ██╔══██╗██╔═══██╗██║██║     ██╔════╝██╔══██╗██╔══██╗██║     ██╔══██╗╚══██╔══╝██╔════╝
+██████╔╝██║     ██║   ██║██╔████╔██║██║██║     █████╔╝     ██████╔╝██║   ██║██║██║     █████╗  ██████╔╝██████╔╝██║     ███████║   ██║   █████╗
+██╔══██╗██║     ██║   ██║██║╚██╔╝██║██║██║     ██╔═██╗     ██╔══██╗██║   ██║██║██║     ██╔══╝  ██╔══██╗██╔═══╝ ██║     ██╔══██║   ██║   ██╔══╝
+██████╔╝███████╗╚██████╔╝██║ ╚═╝ ██║██║███████╗██║  ██╗    ██████╔╝╚██████╔╝██║███████╗███████╗██║  ██║██║     ███████╗██║  ██║   ██║   ███████╗
+╚═════╝ ╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝╚══════╝╚═╝  ╚═╝    ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
+${NC}
+"
+
+if [ "$#" -lt 1 ]; then
     echo "Arguments missing or incomplete. Please follow the prompts."
     promptForInput
 else
-    # Assign arguments to variables
     APP_NAME=$1
-    APP_NAMESPACE=$2
+    APP_NAMESPACE=${2:-}
     BRANCH_NAME=${3:-}
 fi
 
-# Validate the namespace
 if ! isValidAppNamespace "$APP_NAMESPACE"; then
     exit 1
 fi
 
-# Current working directory
 TARGET_DIR=$(pwd)
-
-# Temporary directory for cloning
 TEMP_DIR=$(mktemp -d)
 
-# Clone the specified branch of the repository, or default if not specified
 if [ -z "$BRANCH_NAME" ]; then
     git clone "https://github.com/blumilksoftware/boilerplate" "$TEMP_DIR"
 else
     git clone --branch "$BRANCH_NAME" "https://github.com/blumilksoftware/boilerplate" "$TEMP_DIR"
 fi
 
-# Check if the clone was successful
 if [ $? -ne 0 ]; then
     echo "Failed to clone the repository."
     exit 1
 fi
 
-# Enable dotglob to include hidden files
 shopt -s dotglob
-
-# Move the desired subdirectory to the target directory
 mv "$TEMP_DIR/src/"* "$TARGET_DIR"
-
-# Disable dotglob
 shopt -u dotglob
 
-# Remove the temporary directory
 rm -rf "$TEMP_DIR"
 
-# Change the given string to a new string in all files in the target directory
-# This uses a loop to go through each file and sed to perform the string replacement
 find "$TARGET_DIR" -type f -exec sed -i "s/example-app/$APP_NAME/g" {} \;
 find "$TARGET_DIR" -type f -exec sed -i "s/ExampleApp/$APP_NAMESPACE/g" {} \;
 
-echo "Operation completed."
+echo "Boilerplate copied."
