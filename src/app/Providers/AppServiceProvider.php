01093eac02399627f8d0654b87eb2e72c75218bc
Kamil Stefaniak <kamil.stefaniak@blumilk.pl>
2024-03-18 01:04:46 +0100

    migrate rebuild structure

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -1,11 +1,24 @@
 <?php
 
-declare(strict_types=1);
-
 namespace ExampleApp\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
 }
