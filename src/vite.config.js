c17b10cfd1f4723b87d482771cc4122102a57159
kamil <kamil.piech@collegiumwitelona.pl>
2025-07-16 20:59:17 +0200

    - july updates

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,37 +1,53 @@
-import { defineConfig, loadEnv,  } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import laravel from 'laravel-vite-plugin'
-import { networkInterfaces } from 'os'
+import tailwindcss from '@tailwindcss/vite'
 
 export default ({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-    return defineConfig({
-        build: {
-            outDir: './public/build/',
-        },
-        server: {
-            host: Object.values(networkInterfaces()).flat().find(i => i.family === 'IPv4' && !i.internal).address,
-            port: process.env.VITE_PORT,
-        },
-        resolve: {
-            alias: {
-                '@': '/resources/js',
-            },
-        },
-        plugins: [
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/app.ts'],
-                refresh: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
-                },
-            }),
+  return defineConfig({
+    build: {
+      outDir: './public/build/',
+    },
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: true,
+      origin: 'https://' + process.env.VITE_DEV_SERVER_DOCKER_HOST_NAME,
+      cors: true, // Allow any origin
+      watch: {
+        ignored: [
+          '**/.idea/**',
+          '**/app/**',
+          '**/tests/**',
+          '**/bootstrap/**',
+          '**/public/**',
+          '**/vendor/**',
+          '**/storage/**',
+          '**/node_modules/**',
         ],
-    })
+      },
+    },
+    resolve: {
+      alias: {
+        '@': '/resources/js',
+      },
+    },
+    plugins: [
+      laravel({
+        input: 'resources/js/app.ts',
+        refresh: true,
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
+        },
+      }),
+      tailwindcss(),
+    ],
+  })
 }
